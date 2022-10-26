00caddb461c1923609a8e9d219c3941d6f23605e
asela <asela.atukorala@gmail.com>
2022-10-26 11:20:34 +1030

    got trackID working

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react'
 import './App.css';
 import LyricDisplay from './LyricDisplay';
 import env from "react-dotenv"
+import { useState } from 'react';
 // import fetch from 'node-fetch'
 // import { useState, useEffect } from "react"
 // import useFetch from "react-fetch-hook"
 
-class App extends React.Component {
-  state = {
-    lyrics: '',
-    trackID: '44'
-  }
+function App() {
+  const [lyrics, setLyrics] = useState('')
+  const [trackID, setTrackID] = useState('44')
 
-  handleLyricDisplay = event => {
+  // fetchTrackID = () => {
+  //   fetch("http://api.musixmatch.com/ws/1.1/track.search?apikey=140e78b9724d3b447cc2cbee1a92bf2f&q_artist=toto&q_track=rosanna")
+  //   .then((response) => response.json)
+  //   .then(App => {
+  //     this.setState({ trackID: App })
+  //   })
+  // }
+
+  const handleLyricDisplay = event => {
    // const [lyrics, trackID] = React.useState(null)
     const newContent = event.target.value
     var songTitle = document.getElementById("song-title").value
@@ -22,27 +29,23 @@ class App extends React.Component {
     // const trackID = getTrackID(songTitle, artist)
     const res = fetch("http://api.musixmatch.com/ws/1.1/track.search?apikey=140e78b9724d3b447cc2cbee1a92bf2f&q_artist=toto&q_track=rosanna")
       .then(res => res.json())
-      .then(res => console.log(res['message']['body']['track_list'][0]['track']['track_id']))
+      .then(res => setTrackID(res['message']['body']['track_list'][0]['track']['track_id']))
       // .then(res => this.state.trackID = res['message']['body']['track_list'][0]['track']['track_id'])
-      .then(res => console.log(this.state.trackID))
-
-
-
-    
   }
 
-  render() {
-    const { lyrics } = this.state.lyrics
-
-    return (
-      <div className="App">
-        <LyricDisplay
-          handleLyricDisplay={this.handleLyricDisplay}
-          lyrics={lyrics}
-        />
+    // const { lyrics } = this.state.lyrics
+  return (
+    <div className="App">
+      <LyricDisplay
+        handleLyricDisplay={handleLyricDisplay}
+        lyrics={lyrics}
+      />
+      <div>
+        {trackID}
       </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 async function getTrackID(songTitle, artist) {
